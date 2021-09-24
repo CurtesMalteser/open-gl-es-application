c11591c65acd762b3c33e89ef5dc4e4a8eacd6c8
Curtes Malteser <antonio_bastiao@hotmail.com>
2021-09-24 17:30:20 +0200

    drawing a triangle

---
+++ app/src/main/java/com/curtesmalteser/opengles/glview/shape/Constants.kt
@@ -0,0 +1,7 @@
+package com.curtesmalteser.opengles.glview.shape
+
+/**
+ * Created by António Bastião on 24.09.21
+ */
+// Number of coordinates per vertex in triangle array
+const val COORDS_PER_VERTEX = 3
